e073cb1e64e5849462e25372bf6a643b455f91a8
Piyush Sharma <piyusharmaus@gmail.com>
2025-05-29 17:23:13 -0500

    Initial commit

---
+++ LearningPlatform/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LearningPlatform
+//
+//  Created by Piyush Sharma on 5/29/25.
+//
+
+import SwiftUI
+import FeatureA
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearningPlatform/LearningPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningPlatformApp.swift
+//  LearningPlatform
+//
+//  Created by Piyush Sharma on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
